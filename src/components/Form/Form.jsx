8f65cd70fa1b6387effeefa4a59ebd0e8c6317c6
Arbakov Maxim <fefaodessa@gmail.com>
2022-07-07 18:52:04 +0300

    fix validait

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,9 +14,26 @@ export const FormName = ({ addContact }) => {
     resetForm();
   };
 
+  const nameValid =
+    "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$";
+  const numberValid =
+    /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
+
   let schema = yup.object().shape({
-    name: yup.string().required(),
-    number: yup.number().required(),
+    name: yup
+      .string()
+      .matches(
+        nameValid,
+        'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore dArtagnan'
+      )
+      .required(),
+    number: yup
+      .string()
+      .matches(
+        numberValid,
+        'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+      )
+      .required(),
   });
 
   return (
